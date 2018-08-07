10d7bf1ab30dd4515ad0c7890062fb6b7f05ac5b
Jared Grippe <jared@deadlyicon.com>
2018-08-07 10:49:38 -0700

    reordered the personal_data_keys

--- personal_data_labels.js
+++ personal_data_labels.js
@@ -1,18 +1,18 @@
 'use strict';
 
 module.exports = Object.freeze({
-  birthdate: 'Birthdate',
-  businessphone: 'Buisness Phone',
   email: 'Email',
-  faxnumber: 'Fax Number',
-  firstname: 'First Name',
-  gender: 'Gender',
-  homephone: 'Home Phone Number',
   lastname: 'Surname',
+  firstname: 'First Name',
+  birthdate: 'Birthdate',
+  gender: 'Gender',
   mailingcity: 'City',
   mailingcountry: 'Country',
   mailingpostalcode: 'Postcode / Zip code',
   mailingstate: 'State / Province',
   mailingstreet: 'Address',
+  homephone: 'Home Phone Number',
   mobilephone: 'Mobile Phone Number',
+  faxnumber: 'Fax Number',
+  businessphone: 'Buisness Phone',
 });
