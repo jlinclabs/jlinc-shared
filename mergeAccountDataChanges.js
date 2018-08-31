021c91028497e518b94d28ddc94f76ec7be86aa3
Jared Grippe <jared@deadlyicon.com>
2018-08-31 12:33:48 -0700

    removed deepcopy

--- mergeAccountDataChanges.js
+++ mergeAccountDataChanges.js
@@ -1,22 +1,19 @@
 'use strict';
 
-const deepcopy = require('deepcopy');
-
 const SHARED_PERSONAL_DATA_KEYS = require('./shared_personal_data_keys');
 const PERSONAL_DATA_KEYS = require('./personal_data_keys');
 const CONSENT_KEYS = require('./consents');
 const COMMUNICATION_CHANNELS_KEYS = require('./communication_channels');
 
-module.exports = function({ currentAccountData, stagedChanges, newChanges }){
+module.exports = function({ currentAccountData = {}, stagedChanges = {}, newChanges = {} }){
   const newStagedChanges = {};
-  if (isEmpty(newChanges)) return isEmpty(stagedChanges) ? undefined : deepcopy(stagedChanges);
   SECTION_KEYS.forEach(function([section, keys]){
     const newStagedSection = {};
-    if (!(stagedChanges || {})[section] && !newChanges[section]) return;
+    if (!stagedChanges[section] && !newChanges[section]) return;
     keys.forEach(function(key) {
-      const current = currentAccountData && currentAccountData[section] &&  currentAccountData[section][key];
-      const staged  =      stagedChanges &&      stagedChanges[section] &&       stagedChanges[section][key];
-      const change  =         newChanges &&         newChanges[section] &&          newChanges[section][key];
+      const current = currentAccountData[section] &&  currentAccountData[section][key];
+      const staged  =      stagedChanges[section] &&       stagedChanges[section][key];
+      const change  =         newChanges[section] &&          newChanges[section][key];
       if (staged === undefined && change === undefined) return;
       const newValue = calculateNewStagedValue(section, current, staged, change);
       if (newValue !== undefined) newStagedSection[key] = newValue;
