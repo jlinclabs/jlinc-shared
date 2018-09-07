daa5235532ae780e6301146ad1bf600053c3dcdb
Sylvan Swierkosz <sswierkosz@gmail.com>
2018-09-07 16:56:54 -0700

    adds consent categories

---
+++ consent_categories.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = Object.freeze({
+  'Content Permission': [
+    'Brand Marketing',
+    'Product Marketing',
+    'New Product Marketing',
+    'Specific Product Marketing',
+    'Discount Offers',
+    'Partner Offers',
+    'Customer Research',
+    'Newsletter',
+    'Membership',
+    'Volunteering',
+    'Fundraising',
+  ],
+  'Data Sharing Permission': [
+    'Sharing Data within Group',
+    'Sharing Data with Partners',
+  ],
+  'Data Purpose Permission': [
+    'Automated Decision Making',
+    'Location Tracking',
+    'Online Tracking',
+  ],
+  'Data Management Permission': [
+    'Cross-Border Transfers',
+  ]
+});
