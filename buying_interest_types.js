634c566f1ebccf19c229048fd65d2764f849a7ed
Sylvan Swierkosz <sswierkosz@gmail.com>
2018-09-27 17:15:00 -0700

    renames marketplace_tags to buying_interest_types and adds types, brands, and tags

---
+++ buying_interest_types.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = Object.freeze({
+  'Automotive': {
+    brands: [
+      'Ford',
+      'Volkswagen',
+      'Audi',
+      'Honda',
+      'Mercedes',
+      'BMW',
+      'Jaguar',
+      'Land Rover'
+    ],
+    tags: [
+      '4wd',
+      'diesel',
+      'petrol',
+      '2 door',
+      '4 door',
+      '5 door',
+      '4 seat',
+      '7 seat',
+      'SUV',
+      'Coupe',
+      'Estate',
+      'Automatic',
+      'Manual',
+      'Electric',
+      'Hybrid',
+      'Finance',
+    ],
+  },
+  'Utility': {
+    brands: [
+      'Pacific Gas and Electric Company',
+      'Liberty Utilities',
+      'Ecotricity',
+      'UK Power Networks',
+      'British Gas',
+      'EDF Energy',
+      'Scottish Power',
+    ],
+    tags: [
+      'Payment assistance',
+      'Online payment',
+      'Payment plans',
+      'Bonus offers',
+      'Solar',
+      'Renewable',
+    ],
+  },
+  'General': {
+    brands: null,
+    tags: null,
+  },
+});
