388c5b04a1e278c925b39463a1d31ff68d98146f
Sylvan Swierkosz <sswierkosz@gmail.com>
2018-10-29 15:57:32 -0700

    turns sharing personal data field off in mergeAccountData if personal data field is empty string

--- test/mergeAccountData.spec.js
+++ test/mergeAccountData.spec.js
@@ -37,75 +37,147 @@ describe('mergeAccountData', function(){
     });
   });
 
-  it('should return a merge of the two given accound datas', function(){
-    expect(
-      mergeAccountData(
-        {
-          shared_personal_data: {
-            email: true,
-            firstname: false,
-            salutation: null,
-          },
-          personal_data: {
-            email: 'email',
-            firstname: '',
-          },
-          consents: {
-            'Brand Marketing': true,
-            'New Product Marketing': null,
+  context('when given valid keys', function(){
+    it('should return a merge of the two given accound datas', function(){
+      expect(
+        mergeAccountData(
+          {
+            shared_personal_data: {
+              email: true,
+              firstname: false,
+              salutation: null,
+            },
+            personal_data: {
+              email: 'email',
+              firstname: '',
+            },
+            consents: {
+              'Brand Marketing': true,
+              'New Product Marketing': null,
 
+            },
+            communication_channels: {
+              email_media: { enabled: true },
+              fax_media: { enabled: false },
+              postal_mail_media: { },
+              sms_media: null,
+            },
           },
-          communication_channels: {
-            email_media: { enabled: true },
-            fax_media: { enabled: false },
-            postal_mail_media: { },
-            sms_media: null,
+          {
+            shared_personal_data: {
+              email: false,
+              firstname: null,
+              lastname: true,
+            },
+            personal_data: {
+              email: 'email2',
+              firstname: 'firstname2',
+              lastname: 'lastname2',
+              salutation: '',
+            },
+            consents: {
+              'Brand Marketing': false,
+              'Product Marketing': null,
+            },
+            communication_channels: {
+              email_media: { enabled: false },
+              fax_media: null,
+              social_media: { enabled: true },
+              voice_media: {},
+            },
           },
+        )
+      ).to.deep.equal({
+        shared_personal_data: {
+          email: false,
+          firstname: false,
+          lastname: true,
         },
-        {
-          shared_personal_data: {
-            email: false,
-            firstname: null,
-            lastname: true,
-          },
-          personal_data: {
-            email: 'email2',
-            firstname: 'firstname2',
-            lastname: 'lastname2',
-            salutation: '',
-          },
-          consents: {
-            'Brand Marketing': false,
-            'Product Marketing': null,
-          },
-          communication_channels: {
-            email_media: { enabled: false },
-            fax_media: null,
-            social_media: { enabled: true },
-            voice_media: {},
-          },
+        personal_data: {
+          email: 'email2',
+          firstname: 'firstname2',
+          lastname: 'lastname2',
+          salutation: '',
         },
-      )
-    ).to.deep.equal({
-      shared_personal_data: {
-        email: false,
-        firstname: false,
-        lastname: true,
-      },
-      personal_data: {
-        email: 'email2',
-        firstname: 'firstname2',
-        lastname: 'lastname2',
-        salutation: '',
-      },
-      consents: {
-        'Brand Marketing': false,
-      },
-      communication_channels: {
-        email_media: { enabled: false },
-        fax_media: { enabled: false },
-        social_media: { enabled: true },
-      },
+        consents: {
+          'Brand Marketing': false,
+        },
+        communication_channels: {
+          email_media: { enabled: false },
+          fax_media: { enabled: false },
+          social_media: { enabled: true },
+        },
+      });
+    });
+
+    context('when given new account data that has an enabled shared personal data field but no corresponding personal data value', function(){
+      expect(
+        mergeAccountData(
+          {
+            shared_personal_data: {
+              firstname: true,
+            },
+            personal_data: {
+              firstname: 'firstname',
+            },
+          },
+          {
+            shared_personal_data: {
+              firstname: true,
+            },
+            personal_data: {
+              firstname: '',
+            },
+          },
+        )
+      ).to.deep.equal({
+        shared_personal_data: {
+          firstname: false,
+        },
+        personal_data: {
+          firstname: '',
+        }
+      });
+      expect(
+        mergeAccountData(
+          {},
+          {
+            shared_personal_data: {
+              firstname: true,
+            },
+            personal_data: {
+              firstname: '',
+            },
+          },
+        )
+      ).to.deep.equal({
+        shared_personal_data: {
+          firstname: false,
+        },
+        personal_data: {
+          firstname: '',
+        }
+      });
+      expect(
+        mergeAccountData(
+          {
+            shared_personal_data: {
+              firstname: true,
+            },
+            personal_data: {
+              firstname: '',
+            },
+          },
+          {},
+        )
+      ).to.deep.equal({
+        shared_personal_data: {
+          firstname: false,
+        },
+        personal_data: {
+          firstname: '',
+        }
+      });
     });
   });
 });
