6b3f5a7b77eaaffb42ea4d196d767a14067e2b66
Sylvan Swierkosz <sswierkosz@gmail.com>
2019-02-11 11:36:41 -0800

    updates buying intereset types

--- buying_interest_types.js
+++ buying_interest_types.js
@@ -3,162 +3,51 @@
 module.exports = Object.freeze({
   Automotive: {
     brands: [
+      'Tesla',
+      'Volvo',
+      'Chevy',
       'Ford',
       'Volkswagen',
       'Audi',
       'Honda',
       'Mercedes',
       'BMW',
-      'Jaguar',
-      'Land Rover'
-    ],
-    tags: [
-      '4wd',
-      'diesel',
-      'petrol',
-      '2 door',
-      '4 door',
-      '5 door',
-      '4 seat',
-      '7 seat',
-      'SUV',
-      'Coupe',
-      'Convertible',
-      'Estate',
-      'Automatic',
-      'Manual',
-      'Electric',
-      'Hybrid',
-      'Finance'
     ]
   },
-  Energy: {
-    brands: [
-      'Liberty Utilities',
-      'Ecotricity',
-      'UK Power Networks',
-      'British Gas',
-      'EDF Energy',
-      'Scottish Power',
-      'Eon'
-    ],
-    tags: [
-      'Green energy',
-      'Fixed rate',
-      'Variable rate',
-      'Solar',
-      'Month-to-month',
-      'HVAC service',
-      'Electrical service',
-      'No deposit',
-      'Prepaid',
-      'Commercial'
-    ]
+  Appliances: {
+    brands: []
   },
-  Financial: {
-    brands: [
-      'JP Morgan',
-      'Barclays',
-      'Bank of America',
-      'Charles Schwab',
-      'Capitol One',
-      'Santander',
-      'State Farm',
-      'Wells Fargo',
-      'Geico',
-      'Progressive'
-    ],
-    tags: [
-      'Mobile App',
-      'Checking',
-      'Saving',
-      'Credit Cards',
-      'Home Loans',
-      'Auto Loans',
-      'Student Loans'
-    ]
+  Bikes: {
+    brands: []
   },
-  Retail: {
-    brands: [
-      'Amazon',
-      'Walmart',
-      'IKEA',
-      'Boots',
-      'Walgreens',
-      'Tesco',
-      'ASDA',
-      'H&M',
-      'eBay',
-      'Sainsburys'
-    ],
-    tags: [
-      'T-shirts',
-      'Trousers',
-      'Jeans',
-      'Dresses',
-      'Coffee Mugs',
-      'Shoes',
-      'Appliances',
-      'TVs'
-    ]
+  Clothing: {
+    brands: ['Kate Spade']
   },
-  Telecom: {
-    brands: [
-      'Verizon',
-      'AT&T',
-      'BT',
-      'Vodafone',
-      'Nippon Telegraph & Telephone',
-      'Softbank Group',
-      'Deutsche Telekom AG',
-      'Telefonica S.A.',
-      'America Movil',
-      'China Telecom'
-    ],
-    tags: [
-      'Mobile Phone',
-      'Internet',
-      '5g',
-      '4g',
-      '3g',
-      'DSL',
-      'Fiber',
-      'Satellite',
-      'Cable'
-    ]
+  Construction: {
+    brands: []
   },
-  Nonprofit: {
-    tags: [
-      'Social advocacy',
-      'Charity',
-      'Foundation',
-      'Professional association',
-      'Social club',
-      'Employee beneficiary association',
-      'Community Interest Company',
-      'Cemetery company',
-      'Veterans organizaion',
-      'Withdrawal liability payment fund',
-      'Apostolic association'
-    ]
+  'Consumer Products': {
+    brands: ['P&G','3M Co']
+  },
+  Electronics: {
+    brands: ['Apple']
+  },
+  Food: {
+    brands: ['Ben & Jerry\'s']
+  },
+  Finance: {
+    brands: []
+  },
+  'Heating & Cooling': {
+    brands: []
+  },
+  Lighting: {
+    brands: []
+  },
+  Solar: {
+    brands: ['Tesla']
   },
   Travel: {
-    brands: [
-      'British Airways',
-      'Virgin Atlantic',
-      'American Airlines',
-      'Emirates',
-      'Etihad',
-      'Qantas',
-      'Hilton',
-      'Marriot',
-      'Avis',
-      'Hertz',
-    ],
-    tags: [
-      'Flight',
-      'Hotel',
-      'Car Hire',
-    ]
-  }
+    brands: []
+  },
 });
