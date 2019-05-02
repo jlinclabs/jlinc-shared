92069dc4fae942d142aba1efe9eb7b3d0474557b
Sylvan Swierkosz <sswierkosz@gmail.com>
2019-05-02 15:36:38 -0700

    changes passphrase to password in testPassWordStrength

--- testPasswordStrength.js
+++ testPasswordStrength.js
@@ -2,6 +2,6 @@
 
 module.exports = function testPasswordStrength(password) {
   if (password.length < 12) {
-    return 'Passphrase must be at least 12 characters.';
+    return 'Password must be at least 12 characters.';
   }
 };
