76ac1773af8637ebf0d5ab50036303da1321f31b
Sylvan Swierkosz <sswierkosz@gmail.com>
2019-06-19 15:14:44 -0700

    adds PLEDGE_YEARS and PLEDGE_TYPES

---
+++ carbonpledge.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const PLEDGE_YEARS = Object.freeze([2020, 2025, 2030, 2035, 2040, 2045, 2050]);
+
+const PLEDGE_TYPES = Object.freeze(['Carbon Neutral', '100% Clean Energy', 'Carbon Negative']);
+
+module.exports = {
+  PLEDGE_TYPES,
+  PLEDGE_YEARS,
+};
