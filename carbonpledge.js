8b66576dabec732d265e79e9a2d48a509be6b05d
Sylvan Swierkosz <sswierkosz@gmail.com>
2019-06-21 11:22:55 -0700

    adds isValidPledgeYear and isValidPledgeType to carbonpledge.js

--- carbonpledge.js
+++ carbonpledge.js
@@ -4,7 +4,17 @@ const PLEDGE_YEARS = Object.freeze([2020, 2025, 2030, 2035, 2040, 2045, 2050]);
 
 const PLEDGE_TYPES = Object.freeze(['Carbon Neutral', '100% Clean Energy', 'Carbon Negative']);
 
+function isValidPledgeYear(pledgeYear) {
+  return PLEDGE_YEARS.includes(pledgeYear);
+}
+
+function isValidPledgeType(pledgeType) {
+  return PLEDGE_TYPES.includes(pledgeType);
+}
+
 module.exports = {
   PLEDGE_TYPES,
   PLEDGE_YEARS,
+  isValidPledgeYear,
+  isValidPledgeType,
 };
