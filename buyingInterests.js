c226fe70d39252da71fb8c1ea2db4195b4a7b4f4
Jared Grippe <jared@deadlyicon.com>
2019-09-23 15:27:18 -0700

    added buyingInterests.js

---
+++ buyingInterests.js
@@ -0,0 +1,130 @@
+'use strict';
+
+const DATE_STRING = /^\d\d\d\d-\d\d-\d\d$/;
+const VALID_CURRENCIES = Object.freeze(['$', '€', '£', '¥']);
+
+// this is designed to take a record from postgres and normalize it
+function normalizeBuyingInterest(buyingInterest){
+  return {
+    uid: nullToUndefined(buyingInterest.uid),
+    type: nullToUndefined(buyingInterest.type),
+    organization_apikey: nullToUndefined(buyingInterest.organization_apikey),
+    description: nullToUndefined(buyingInterest.description),
+    location: nullToUndefined(buyingInterest.location),
+    price_low: nullToUndefined(buyingInterest.price_low),
+    price_high: nullToUndefined(buyingInterest.price_high),
+    beginning_date: dateToString(buyingInterest.beginning_date),
+    end_date: dateToString(buyingInterest.end_date),
+    industry: nullToUndefined(buyingInterest.industry),
+    currency: nullToUndefined(buyingInterest.currency),
+    brands: buyingInterest.brands ? Array.from(buyingInterest.brands) : [],
+    tags: buyingInterest.tags ? Array.from(buyingInterest.tags) : [],
+    created: nullToUndefined(buyingInterest.created),
+  };
+}
+
+function nullToUndefined(value){
+  return value === null ? undefined : value;
+}
+
+function dateToString(date){
+  return date instanceof Date
+    ? `${date.getFullYear()}-${zp(date.getUTCMonth() + 1)}-${zp(date.getUTCDate())}`
+    : undefined;
+}
+
+const zp = n => n.toString().length === 1 ? `0${n}` : n;
+
+// this function is designed to be used on the a-api and b-api to validate
+// the object posted to the server to create a buying interest
+function validateBuyingInterest(buyingInterest){
+  if (!buyingInterest) throw new Error(`buyingInterest required`);
+
+  if (typeof buyingInterest !== 'object') {
+    throw new Error(`buyingInterest must be an object`);
+  }
+
+  const {
+    description,
+    currency,
+    price_low,
+    price_high,
+    beginning_date,
+    end_date,
+    location,
+    brands,
+    tags,
+  } = buyingInterest;
+
+  if (typeof description !== 'string') throw new Error(`Description cannot be blank`);
+
+  if (description.length < 10) throw new Error(`Description must at least 10 characaters long`);
+
+  if (!VALID_CURRENCIES.includes(currency)) {
+    throw new Error(`Currency must be one of ${VALID_CURRENCIES.join(',')}`);
+  }
+
+  if (!Array.isArray(tags)) throw new Error(`Tags must be an array`);
+  if (!isArrayOfNonEmptyStrings(tags))
+    throw new Error('Tags can only be non-empty strings');
+
+  if (tags.length < 1) throw new Error('At least one tag is required');
+
+  if (typeof price_low !== 'undefined' && (!isInteger(price_low) || price_low < 0))
+    throw new Error(`Minimum price must be a positive integer`);
+
+  if (typeof price_high !== 'undefined' && (!isInteger(price_high) || price_high < 0))
+    throw new Error(`Maximum price must be a positive integer`);
+
+  if (isInteger(price_low) && isInteger(price_high) && price_low > price_high)
+    throw new Error(`Maximum price must be greater than or equal to Minimum price`);
+
+  if (typeof beginning_date !== 'undefined'){
+    if (typeof beginning_date !== 'string' || !isDateString(beginning_date))
+      throw new Error(`Beginning date must be a valid date string`);
+  }
+
+  if (typeof end_date !== 'undefined'){
+    if (typeof end_date !== 'string' || !isDateString(end_date))
+      throw new Error(`End date must be a valid date string`);
+  }
+
+  if (
+    typeof beginning_date === 'string' &&
+    typeof end_date === 'string' &&
+    new Date(end_date) < new Date(beginning_date)
+  )
+    throw new Error(`End date must greater than or equal to Beginning date`);
+
+  if (typeof location !== 'undefined'){
+    if (typeof location !== 'string') throw new Error(`Location must be a string`);
+    if (location.length < 4) throw new Error(`Location must be 4 or more characters`);
+  }
+
+  if (typeof brands !== 'undefined'){
+    if (!Array.isArray(brands)) throw new Error('Brands must be an array');
+    if (!isArrayOfNonEmptyStrings(brands))
+      throw new Error('Brands can only be non-empty strings');
+  }
+};
+
+function isInteger(n){
+  return typeof n === 'number' && !n.toString().includes('.');
+}
+
+function isDateString(date){
+  return (
+    typeof date === 'string' &&
+    date.match(DATE_STRING) &&
+    dateToString(new Date(date)) === date
+  );
+}
+
+function isArrayOfNonEmptyStrings(array){
+  return array.every(member => typeof member === 'string' && member.length > 1);
+}
+
+module.exports = {
+  normalizeBuyingInterest,
+  validateBuyingInterest,
+};
---
+++ test/buyingInterests.spec.js
@@ -0,0 +1,333 @@
+'use strict';
+
+const buyingInterests = require('../buyingInterests');
+
+describe('buyingInterests', function() {
+  it('should have helper functions', function(){
+    expect(buyingInterests).to.be.an('object');
+    expect(buyingInterests.normalizeBuyingInterest).to.be.a('function');
+    expect(buyingInterests.validateBuyingInterest).to.be.a('function');
+  });
+  describe('normalizeBuyingInterest', function() {
+    const { normalizeBuyingInterest } = buyingInterests;
+    it('should convert beginning_date and end_date into strings and prune keys', function(){
+      expect(normalizeBuyingInterest({})).to.deep.equal({
+        uid: undefined,
+        type: undefined,
+        organization_apikey: undefined,
+        description: undefined,
+        location: undefined,
+        price_low: undefined,
+        price_high: undefined,
+        beginning_date: undefined,
+        end_date: undefined,
+        industry: undefined,
+        currency: undefined,
+        brands: [],
+        tags: [],
+        created: undefined,
+      });
+
+      expect(normalizeBuyingInterest({
+        uid: null,
+        type: null,
+        organization_apikey: null,
+        description: null,
+        location: null,
+        price_low: null,
+        price_high: null,
+        beginning_date: null,
+        end_date: null,
+        industry: null,
+        currency: null,
+        brands: null,
+        tags: null,
+        created: null,
+      })).to.deep.equal({
+        uid: undefined,
+        type: undefined,
+        organization_apikey: undefined,
+        description: undefined,
+        location: undefined,
+        price_low: undefined,
+        price_high: undefined,
+        beginning_date: undefined,
+        end_date: undefined,
+        industry: undefined,
+        currency: undefined,
+        brands: [],
+        tags: [],
+        created: undefined,
+      });
+
+      expect(normalizeBuyingInterest({
+        uid: 'thisisafakeuuid',
+        type: 'business',
+        organization_apikey: 'jlinclabs',
+        description: 'a tiny red car with 4 doors',
+        location: 'Nevada, CA',
+        price_low: 1400000,
+        price_high: 15000000,
+        beginning_date: new Date('1982-08-23'),
+        end_date: new Date('1984-08-01'),
+        industry: 'automobiles',
+        currency: '$',
+        brands: ['myada','porche'],
+        tags: new Set(['red', '4 door']),
+        created: new Date('1982-08-23'),
+      })).to.deep.equal({
+        uid: 'thisisafakeuuid',
+        type: 'business',
+        organization_apikey: 'jlinclabs',
+        description: 'a tiny red car with 4 doors',
+        location: 'Nevada, CA',
+        price_low: 1400000,
+        price_high: 15000000,
+        beginning_date: '1982-08-23',
+        end_date: '1984-08-01',
+        industry: 'automobiles',
+        currency: '$',
+        brands: ['myada','porche'],
+        tags: ['red', '4 door'],
+        created: new Date('1982-08-23'),
+      });
+    });
+  });
+  describe('validateBuyingInterest', function() {
+    it('should validate a buying interest JSON object', function(){
+      // this help simulates being given an object parsed from json
+      const validateBuyingInterest = object =>
+        buyingInterests.validateBuyingInterest(object ? JSON.parse(JSON.stringify(object)) : undefined);
+
+      expect(() => validateBuyingInterest()).to.throw(Error, `buyingInterest required`);
+
+      expect(() => validateBuyingInterest(452)).to.throw(Error, `buyingInterest must be an object`);
+
+      expect(() => validateBuyingInterest({})).to.throw(Error, `Description cannot be blank`);
+
+      expect(() => validateBuyingInterest({
+        description: 'shoe',
+      })).to.throw(Error, `Description must at least 10 characaters long`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+      })).to.throw(Error, `Currency must be one of $,€,£,¥`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+      })).to.throw(Error, `Tags must be an array`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: [],
+      })).to.throw(Error, `At least one tag is required`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: [123],
+      })).to.throw(Error, `Tags can only be non-empty strings`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: [''],
+      })).to.throw(Error, `Tags can only be non-empty strings`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: [null],
+      })).to.throw(Error, `Tags can only be non-empty strings`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+      })).to.not.throw();
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        price_low: '$10.00',
+      })).to.throw(Error, `Minimum price must be a positive integer`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        price_low: '10.00',
+      })).to.throw(Error, `Minimum price must be a positive integer`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        price_low: -11,
+      })).to.throw(Error, `Minimum price must be a positive integer`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        price_low: 11.34,
+      })).to.throw(Error, `Minimum price must be a positive integer`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        price_low: 1134,
+      })).to.not.throw();
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        price_low: 1134,
+        price_high: -11,
+      })).to.throw(Error, `Maximum price must be a positive integer`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        price_low: 1134,
+        price_high: 11.34,
+      })).to.throw(Error, `Maximum price must be a positive integer`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        price_low: 1134,
+        price_high: 823,
+      })).to.throw(Error, `Maximum price must be greater than or equal to Minimum price`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        price_low: 1134,
+        price_high: 1134,
+      })).to.not.throw();
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        beginning_date: new Date,
+      })).to.throw(Error, `Beginning date must be a valid date string`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        beginning_date: 232323,
+      })).to.throw(Error, `Beginning date must be a valid date string`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        beginning_date: 'yesturday',
+      })).to.throw(Error, `Beginning date must be a valid date string`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        beginning_date: '1982-99-99',
+      })).to.throw(Error, `Beginning date must be a valid date string`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        beginning_date: '1982-08-99',
+      })).to.throw(Error, `Beginning date must be a valid date string`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        beginning_date: '1982-08-23',
+      })).to.not.throw();
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        end_date: 'yesturday',
+      })).to.throw(Error, `End date must be a valid date string`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        end_date: '1982-99-99',
+      })).to.throw(Error, `End date must be a valid date string`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        end_date: '1982-08-99',
+      })).to.throw(Error, `End date must be a valid date string`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        end_date: '1982-08-23',
+      })).to.not.throw();
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        beginning_date: '1982-08-23',
+        end_date: '1982-08-22',
+      })).to.throw(Error, `End date must greater than or equal to Beginning date`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        location: '',
+      })).to.throw(Error, `Location must be 4 or more characters`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        location: 'my feet',
+      })).to.not.throw();
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        brands: '',
+      })).to.throw(Error, `Brands must be an array`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        brands: [123],
+      })).to.throw(Error, `Brands can only be non-empty strings`);
+
+      expect(() => validateBuyingInterest({
+        description: 'A new pair of red shoes.',
+        currency: '£',
+        tags: ['red'],
+        brands: ['Nike'],
+      })).to.not.throw();
+
+    });
+  });
+});
