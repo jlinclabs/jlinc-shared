164bab41d5ce7e256efdf868b00fe060a563aef4
Sylvan Swierkosz <sswierkosz@gmail.com>
2019-10-22 15:37:51 -0700

    adds organization_types_labels

---
+++ organization_types_labels.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = Object.freeze({
+  'ngo': 'Non-profit entity (NGO)',
+  'corporation': 'For-profit business or corp',
+  'government': 'Government',
+  'academic_entity': 'Academic Entity',
+  'informal_organization': 'Informal Organization',
+  'individual_thought_leader': 'Individual Thought Leader',
+});
