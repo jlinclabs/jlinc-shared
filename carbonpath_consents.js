6e88179d042568233f1ab31a8bf82da92332e97d
Sylvan Swierkosz <sswierkosz@gmail.com>
2019-11-05 12:28:47 -0800

    adds carbon_consents

---
+++ carbonpath_consents.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = Object.freeze([
+  'Brand Marketing',
+  'Product Marketing',
+  'Partner Offers',
+  'Customer Research',
+  'Newsletter',
+  'Membership',
+  'Volunteering',
+  'Fundraising',
+  'Automated Decision Making',
+  'Location Tracking',
+  'Online Tracking',
+]);
--- test/consents.spec.js
+++ test/consents.spec.js
@@ -1,11 +1,20 @@
 'use strict';
 
-const CONSENTS = require('../consents');
+const VALID_CONSENT_KEYS = require('../consents');
+const VALID_CARBONPATH_CONSENT_KEYS = require('../carbonpath_consents');
 
 describe('consents', function() {
   it('should not have duplicates', function() {
-    for (const consent of CONSENTS){
-      expect(CONSENTS.filter(c => c === consent)).to.have.lengthOf(1);
+    for (const consent of VALID_CONSENT_KEYS){
+      expect(VALID_CONSENT_KEYS.filter(c => c === consent)).to.have.lengthOf(1);
+    }
+  });
+});
+
+describe('carbon consents', function() {
+  it('should all be valid consents', function() {
+    for (const consent of VALID_CARBONPATH_CONSENT_KEYS) {
+      expect(VALID_CONSENT_KEYS.includes(consent)).to.be.true;
     }
   });
 });
