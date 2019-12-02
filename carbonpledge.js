ba649f782c12f708f798fa019b9dc9cfaaba4ee8
Jared Grippe <jared@deadlyicon.com>
2019-12-02 15:47:20 -0800

    renamed 'Carbon Negative' to 'Carbon Removal'

--- carbonpledge.js
+++ carbonpledge.js
@@ -1,8 +1,14 @@
 'use strict';
 
-const PLEDGE_YEARS = Object.freeze([2020, 2025, 2030, 2035, 2040, 2045, 2050]);
+const PLEDGE_YEARS = Object.freeze([
+  2020, 2025, 2030, 2035, 2040, 2045, 2050,
+]);
 
-const PLEDGE_TYPES = Object.freeze(['Carbon Neutral', '100% Clean Energy', 'Carbon Negative']);
+const PLEDGE_TYPES = Object.freeze([
+  'Carbon Neutral',
+  '100% Clean Energy',
+  'Carbon Removal',
+]);
 
 function isValidPledgeYear(pledgeYear) {
   return PLEDGE_YEARS.includes(pledgeYear);
