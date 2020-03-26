4f9a9556185dec08af7545bd237a22c1d1ae20cb
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-03-25 17:04:40 -0700

    updates and adds new org types

--- organization_types_labels.js
+++ organization_types_labels.js
@@ -1,10 +1,13 @@
 'use strict';
 
 module.exports = Object.freeze({
-  'ngo': 'Non-profit entity (NGO)',
-  'corporation': 'For-profit business or corp',
-  'government': 'Government',
-  'academic_entity': 'Academic Entity',
-  'informal_organization': 'Informal Organization',
-  'individual_thought_leader': 'Individual Thought Leader',
+  'ngo': 'Non-profit (NGO)',
+  'corporation': 'Corporate Enterprise',
+  'government': 'Government / Political',
+  'academic_entity': 'Academic / Educational',
+  'informal_organization': 'Group',
+  'individual_thought_leader': 'Individual',
+  'small_business': 'Small Business',
+  'association': 'Association',
+  'faithbased_religious': 'Faith-based / Religious',
 });
