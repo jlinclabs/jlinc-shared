1c5dfe69eb744ffd9377e9d8d30a0cde8875d538
Jared Grippe <jared@deadlyicon.com>
2020-05-05 17:01:32 -0700

    removed carbonpath related stuffs

--- test/consents.spec.js
+++ test/consents.spec.js
@@ -1,20 +1,11 @@
 'use strict';
 
 const VALID_CONSENT_KEYS = require('../consents');
-const VALID_CARBONPATH_CONSENT_KEYS = require('../carbonpath_consents');
 
 describe('consents', function() {
   it('should not have duplicates', function() {
     for (const consent of VALID_CONSENT_KEYS){
       expect(VALID_CONSENT_KEYS.filter(c => c === consent)).to.have.lengthOf(1);
-    }
-  });
-});
-
-describe('carbon consents', function() {
-  it('should all be valid consents', function() {
-    for (const consent of VALID_CARBONPATH_CONSENT_KEYS) {
-      expect(VALID_CONSENT_KEYS.includes(consent)).to.be.true;
     }
   });
 });
