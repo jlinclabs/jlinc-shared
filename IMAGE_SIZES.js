3f7a1135191684403ad39eae44267beaf226d757
Jared Grippe <jared@deadlyicon.com>
2020-07-16 18:04:24 -0700

    fix lint errors

--- IMAGE_SIZES.js
+++ IMAGE_SIZES.js
@@ -1,6 +1,8 @@
+'use strict';
+
 // These are the display sizes. We double these when storing for rentina displays
 module.exports = {
   AVATAR: {height: 150, width: 150},
   ICON: {height: 150, width: 150},
   BANNER: {height: 360, width: 820},
-}
+};
--- assets.js
+++ assets.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const request = require('request-promise');
 
 const {
@@ -12,7 +14,7 @@ async function createAssetUploadToken(){
 };
 
 async function uploadAsset({ uploadToken, value, filename }){
-  const url = `${ASSETS_SERVER_URL}/upload/${uploadToken}`
+  const url = `${ASSETS_SERVER_URL}/upload/${uploadToken}`;
   return await request.post(url, {
     json: true,
     formData: {
@@ -21,21 +23,21 @@ async function uploadAsset({ uploadToken, value, filename }){
         options: { filename }
       },
     },
-  })
+  });
 }
 
 function dataURIToAsset(dataURI) {
-  const matches = dataURI.match(/^data:image\/([^;]+?);(base64|charset=UTF-8),(.+)$/)
-  if (!matches) throw new Error('dataURI in unsupported format')
-  let [, type, encoding, data] = matches
+  const matches = dataURI.match(/^data:image\/([^;]+?);(base64|charset=UTF-8),(.+)$/);
+  if (!matches) throw new Error('dataURI in unsupported format');
+  let [, type, encoding, data] = matches;
   if (type === 'svg+xml'){
-    type = 'svg'
-    data = decodeURIComponent(data)
+    type = 'svg';
+    data = decodeURIComponent(data);
   }
-  let filename = 'image.'+type
-  if (encoding === 'charset=UTF-8') encoding = 'utf8'
-  let value = Buffer.from(data, encoding)
-  return { value, filename }
+  let filename = 'image.'+type;
+  if (encoding === 'charset=UTF-8') encoding = 'utf8';
+  let value = Buffer.from(data, encoding);
+  return { value, filename };
 }
 
 async function uploadImageDataURL(imageDataURL){
@@ -43,12 +45,12 @@ async function uploadImageDataURL(imageDataURL){
   const { url } = await uploadAsset({
     uploadToken,
     ...dataURIToAsset(imageDataURL),
-  })
+  });
   return url;
 }
 
 function isAssetUrl(url){
-  return typeof url === 'string' && url.startsWith(ASSETS_SERVER_URL)
+  return typeof url === 'string' && url.startsWith(ASSETS_SERVER_URL);
 }
 
 module.exports = {
@@ -57,4 +59,4 @@ module.exports = {
   dataURIToAsset,
   uploadImageDataURL,
   isAssetUrl,
-}
+};
