5eba9866d49f8f41cb49a74b296085983220520e
Jared Grippe <jared@deadlyicon.com>
2020-07-21 15:09:08 -0700

    Node{,s}->Hub{,s}

--- PNFO.js
+++ PNFO.js
@@ -15,6 +15,6 @@
 */
 
 module.exports = Object.freeze({
-  singular: 'Node',
-  plural: 'Nodes',
+  singular: 'Hub',
+  plural: 'Hubs',
 });
--- test/PNFO.spec.js
+++ test/PNFO.spec.js
@@ -7,12 +7,12 @@ describe('PNFO', function(){
     expect(PNFO).to.be.frozen;
     expect(PNFO.singular).to.be.frozen;
     expect(PNFO.plural).to.be.frozen;
-    expect(PNFO).to.deep.equal({singular: 'Node', plural: 'Nodes'});
-    expect(PNFO.singular).to.equal('Node');
-    expect(PNFO.plural).to.equal('Nodes');
+    expect(PNFO).to.deep.equal({singular: 'Hub', plural: 'Hubs'});
+    expect(PNFO.singular).to.equal('Hub');
+    expect(PNFO.plural).to.equal('Hubs');
     expect(() => { PNFO.singular = 'Group'; }).to.throw();
     expect(() => { PNFO.plural = 'Groups'; }).to.throw();
-    expect(PNFO.singular).to.equal('Node');
-    expect(PNFO.plural).to.equal('Nodes');
+    expect(PNFO.singular).to.equal('Hub');
+    expect(PNFO.plural).to.equal('Hubs');
   });
 });
