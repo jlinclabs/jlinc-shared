c21b930a91bc96cd9602c685f3288969226a38f4
Jared Grippe <jared@deadlyicon.com>
2023-04-25 12:10:10 -0700

    replaced random with static values in the crypto tests so we can swap out libsodium libraries with greater confidence

--- crypto.spec.js
+++ crypto.spec.js
@@ -28,6 +28,7 @@ describe('crypto', function() {
   });
 
   describe('encrypt and decrypt', function() {
+    const secret = 'eZLUEzOYaDMUxDGGvlgmgdRWF8nrPhCyrwczoznH8VA';
     let encrypt;
     let decrypt;
     let encryptString;
@@ -38,19 +39,20 @@ describe('crypto', function() {
         decrypt,
         encryptString,
         decryptString,
-      } = createCrypto(createSecret(32)));
+      } = createCrypto(secret));
     });
 
     describe('encrypt and decrypt', function() {
       it('should encrypt and decrypt', function(){
-        const secret = createSecret();
-        const message = `the time is ${Date.now()}`;
+        const secret = 'ciMRglkN5c0uo6IeBr9xtjsAc6acy88z';
+        const message = `the is some static message`;
         const encodedMessage = b64.encode(Buffer.from(message));
+        expect(encodedMessage).to.equal(`dGhlIGlzIHNvbWUgc3RhdGljIG1lc3NhZ2U`);
 
         const encryptedMessage = encrypt(encodedMessage, secret);
+        expect(encryptedMessage).to.equal('AAAAAAAAAAAAAAAAAAAAAL4mdVgMsJxLxWtpmSxlxZy9DR9JKECC2IxjFfJfWb_baiVau5rvOJpiAA');
         expect(encryptedMessage).to.be.a('string');
         expect(encryptedMessage).to.not.equal(message);
-
         expect(decrypt(encryptedMessage, createSecret())).to.be.undefined;
 
         const decryptedMessage = decrypt(encryptedMessage, secret);
@@ -59,7 +61,7 @@ describe('crypto', function() {
       });
 
       it('should be able to handle JSON strings', function(){
-        const secret = createSecret();
+        const secret = 'jfSmabHkulpu0PKGS4mD79efxnsxwHzc';
         const data = {
           some: 'json',
           size: 45,
@@ -68,20 +70,31 @@ describe('crypto', function() {
         };
         const json = JSON.stringify(data);
         const encryptedJson = encrypt(b64.encode(Buffer.from(json)), secret);
+        expect(encryptedJson).to.equal('AAAAAAAAAAAAAAAAAAAAAMz6dz2jcsgBzUp4xC1kWWO4vR2c557TAUYy5exw5bedY4Cr0SUIW2gtnfIWNuRPXqDVdb5F3kw2fAr5oosGObZ4PG4NDdLgTimvP3l1OQUXRvII-Vnr');
         const decryptedJson = b64.decode(decrypt(encryptedJson, secret)).toString();
         expect(decryptedJson).to.equal(json);
         expect(JSON.parse(decryptedJson)).to.deep.equal(data);
+      });
+
+
+      it('static encrypted values', function(){
+        const secret = 'nsQigp8iCpTvb_RH9eZcCJdhLGBgQu3h';
+        expect(
+          decryptString('AAAAAAAAAAAAAAAAAAAAAHXxgwtGMCPjuN8QPFjW7kqvSQ0pWYreqqOttOqsJ4yMtH5mTYUzoyKa0A', secret)
+        ).to.equal('{"victor":{"badass":true}}');
       });
     });
 
     describe('encryptString and decryptString', function() {
       it('should encrypt and decrypt', function(){
-        const secret = createSecret();
-        const string = `some random test string ${Date.now()}`;
-        expect(decryptString(encryptString(string, secret), secret)).to.equal(string);
+        const secret = '7QpEa2kqTg7T7m-qRRJyO9FBcOw-C44t';
+        const string = `some static test string`;
+        const encryptedString = encryptString(string, secret);
+        expect(encryptedString).to.equal('AAAAAAAAAAAAAAAAAAAAAIlVSz8wHBqapDsktOlWdRQD7SS0zNbLxtB3bOYFJNXyOdTnooXXOg');
+        expect(decryptString(encryptedString, secret)).to.equal(string);
       });
       it('should be able to handle JSON strings', function(){
-        const secret = createSecret();
+        const secret = 'X9ETsvUyrGTwN97V1oLDuCkRt27vNFjb';
         const data = {
           some: 'json',
           size: 45,
@@ -90,10 +103,18 @@ describe('crypto', function() {
         };
         const json = JSON.stringify(data);
         const encryptedJson = encryptString(json, secret);
+        expect(encryptedJson).to.equal('AAAAAAAAAAAAAAAAAAAAAGF_ha6z7d1c37HqcGX1ExxBCVhnQzrsObIHzOrOUc_n5JS7sRCTvSI-Wxx6ko2zHSSjMNP8Wag1JopVonQUr-7sjyeoShDFmDyikkqRLk9s-jxXqt4L');
         const decryptedJson = decryptString(encryptedJson, secret);
         expect(decryptedJson).to.equal(json);
         expect(JSON.parse(decryptedJson)).to.deep.equal(data);
       });
+      it('static encrypted values', function(){
+        const secret = 'kdpsVNaNHP4u7Ksc6gdu9ST9agIB1KVg';
+        expect(decryptString('AAAAAAAAAAAAAAAAAAAAAHh8IpF6C7imsXC-SH20HwxXePW6W2Oh-pIPYlHkm5_7VzH1ok_YG5K7xJ-h', secret)).to.equal('jlinc connects all the silos');
+        expect(decryptString('AAAAAAAAAAAAAAAAAAAAANbWqXtxClJzpKGs48WpYsVGNuq9Wzet595bfBDyidv7SC737FPPToSv1Q', secret)).to.equal('{"victor":{"badass":true}}');
+      });
     });
+
   });
+
 });
